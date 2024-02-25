0af512882372f7e1da8f9cd3c8b3d2afdb531231
Naufal <naufalkurniawan605@gmail.com>
2024-02-25 14:01:45 +0700

    create product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('product.index');
+    }
+    public function food_beverage(){
+        return view('product.food-beverage');
+    }
+    public function beauty_health(){
+        return view('product.beauty-health');
+    }
+    public function home_care(){
+        return view('product.home-care');
+    }
+    public function baby_kid(){
+        return view('product.baby-kid');
+    }
+}
