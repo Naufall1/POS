092d21485027a7fe0dc43bc0d1f96fca890adc58
Naufal <naufalkurniawan605@gmail.com>
2024-02-25 14:02:43 +0700

    add all routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Melakukan redirect ke halaman home ketika ada yang mengakses root path
+ */
+Route::redirect('/', '/home');
+
+/**
+ * Routing url /home ke HomeController
+ */
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function (){
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/food-beverage', [ProductController::class, 'food_beverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beauty_health']);
+    Route::get('/home-care', [ProductController::class, 'home_care']);
+    Route::get('/baby-kid', [ProductController::class, 'baby_kid']);
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'detail']);
+
+Route::get('/sales', [SalesController::class, 'index']);
